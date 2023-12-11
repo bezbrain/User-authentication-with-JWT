2bb9532d2d025a8ffe66503322814863a1c52f2b
bezbrain <belloquadriolawale@gmail.com>
2023-12-11 09:39:06 +0100

    Use package to handle the status code

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,7 @@
+const { StatusCode } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({
+  return res.status(StatusCode.INTERNAL_SERVER_ERROR).json({
     success: false,
     message: err,
   });
